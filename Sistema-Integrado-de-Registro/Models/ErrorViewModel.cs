d16910b7675d05a77544e0db3f1f68d964b7389e
williamhdz94 <williamhernandez450@gmail.com>
2025-05-05 20:49:02 -0600

    Agregar archivos de proyecto.

---
+++ Sistema-Integrado-de-Registro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Integrado_de_Registro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
